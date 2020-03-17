2a1c77eb86e8c37989d73ac8160d5752d4097c64
sanal1989 <55034623+sanal1989@users.noreply.github.com>
2020-03-17 13:58:14 +0300

    unit complete:16 error:4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let count = 0;
+  for(let i = 0; i < str.length; i++){
+    if(str[i]=="("){
+      count++;
+    }else if(str[i]=="["){
+      count++;
+    }else if(str[i]=="{"){
+      count++;
+    }else if(str[i]==")"){
+      count--;
+    }else if(str[i]=="]"){
+      count--;
+    }else if(str[i]=="}"){
+      count--;
+    }
+    if(count <=-1 ){
+      return false;
+    }
+  }
+  return count == 0;
 }
