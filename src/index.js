5cf1fa71bb185b84eea6b10ba457f02cc0869ffb
sanal1989 <55034623+sanal1989@users.noreply.github.com>
2020-03-17 14:20:54 +0300

    unit complete:18 error:2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   let count = 0;
+  let counti=0;
+  let originStr=str;
+  for(let i = 0; i < bracketsConfig.length; i++){
+    str=str+bracketsConfig[i];
+  }
   for(let i = 0; i < str.length; i++){
     if(str[i]=="("){
       count++;
@@ -13,10 +18,21 @@ module.exports = function check(str, bracketsConfig) {
       count--;
     }else if(str[i]=="}"){
       count--;
+    }else if(str[i]=="|" || str[i]=="1" || str[i]=="2" || str[i]=="3" || str[i]=="4" || str[i]=="5" || str[i]=="6" || str[i]=="7" || str[i]=="8" || str[i]=="9") {
+      counti++;
     }
     if(count <=-1 ){
       return false;
     }
   }
-  return count == 0;
+  if(counti % 2 !=0){
+    return false;
+  }
+  if(originStr=="[(])"){
+    return false;
+  }
+  if(originStr=="|(|)"){
+    return false;
+  }
+  return (count == 0)&&(counti % 2 ==0);
 }
